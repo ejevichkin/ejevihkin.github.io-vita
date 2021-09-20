c36b1e07328c5a5e0776e103d8c701234c07709a
ejevichkin <88722239+ejevichkin@users.noreply.github.com>
2021-09-21 02:58:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-// $(document).ready(function(){
-//     $('.carousel__inner').slick({
-//         speed: 1300,
-// //         adaptiveHeight: true,    
-//         prevArrow: '<button type="button" class="slick-prev"><img src="./icons/left.svg"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="./icons/right.svg"></button>',
-
-//     });
-//  });
-
-// const slider = tns({
-//     container: '.carousel__inner',
-//     items: 1,
-//     slideBy: 'page',
-//     autoplay: false,
-//     controls: false,
-//     nav: true,
-//     responsive: {
-//         640: {
-//             nav: true,
-//             items: 1
-//         },
-//         700: {
-//             nav: true,
-//         },
-//         900: {
-//             nav: false,
-//             items: 1
-//         }
-//     }
-// });
-
-// document.querySelector('.prev').addEventListener('click',function () {
-//     slider.goTo('prev');
-// });
-// document.querySelector('.next').addEventListener('click',function () {
-//     slider.goTo('next');
-// });
 
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__menu'),
@@ -62,91 +24,79 @@ $(document).ready(function(){
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-//     function toggleSlide(item) {
-//         $(item).each(function(i) {
-//             $(this).on('click', function(e) {
-//                 e.preventDefault();
-//                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-//                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-//             })
-//         });
-//     };
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
 
-//     toggleSlide('.catalog-item__link');
-//     toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 
-//     //modal
+$('input[name=phone').mask("+7(999)999-99-99");
 
-//     $('[data-modal=consultation]').on('click', function() {
-//         $('.overlay, #consultation').fadeIn('slow');
-//     });
-//     $('.modal__close').on('click', function() {
-//         $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-//     });
+//modal
 
-//     $('.button_mini').each(function(i) {
-//         $(this).on('click', function() {
-//             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-//             $('.overlay, #order').fadeIn('slow');
-//         })
-//     });
+    // $('[data-modal="thanks"]').on('click',function() {
+    //     $('.overlay, #thanks').fadeIn('slow');
+    // }); 
+    $('.modal__close').on('click',function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
 
-//     function validateForms(form){
-//         $(form).validate({
-//             rules: {
-//                 name: "required",
-//                 phone: "required",
-//                 email: {
-//                     required: true,
-//                     email: true
-//                 }
-//             },
-//             messages: {
-//                 name: "Введите свое имя",
-//                 phone: "Введите свой номер телефона",
-//                 email: {
-//                   required: "Введите свою почту",
-//                   email: "Неправильно введен адрес почты"
-//                 }
-//               }
-//         });
-//     };
-//     validateForms('#consultation-form');
-//     validateForms('#consultation form');
-//     validateForms('#order form');
-    
-//     $('input[name=phone]').mask("+7 (999) 999-9999");
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 
-//     $('form').submit(function(e) {
-//         e.preventDefault();
-//         $.ajax({
-//             type: "POST",
-//             url: "mailer/smart.php",
-//             data: $(this).serialize()
-//         }).done(function() {
-//             $(this).find("input").val("");
-//             $('#consultation, #order').fadeOut();
-//             $('.overlay, #thanks').fadeIn('slow');
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Введите название лекарства",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите Ваш номер телефона",
+                
+            }
+        });
+    };
 
-//             $('form').trigger('reset');
-//         });
-//         return false;
-//     });
-    
-//     //scroll and pageup
+    validateForms('#consultation-form');
+    validateForms('#call_me');
+    validateForms('#order form');
 
-//     $(window).scroll(function() {
-//         if($(this).scrollTop() > 1600) {
-//             $('.pageup').fadeIn();
-//         } else {
-//             $('.pageup').fadeOut();
-//         }
-//     });
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
-//     $("a[href^='#up']").click(function() {
-//         const _href = $(this).attr("href");
-//         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
-//         return false;
-//     });
+            $('form').trigger('reset');
+        });
+        return false;
+    });
     
 }); 
